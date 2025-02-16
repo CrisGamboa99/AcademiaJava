1d608f3bac8eb8422b1fa4849932bfa9cbf74abf
CrisGamboa99 <cristian.gamboa.99@outlook.com>
2025-02-15 19:22:01 -0600

    terminando programa 5

--- Semana 5/Programas/programa5/src/main/java/com/ejemplo/spring_boot_mongodb/controller/EmpleadoController.java
+++ Semana 5/Programas/programa5/src/main/java/com/ejemplo/spring_boot_mongodb/controller/EmpleadoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,11 @@ public class EmpleadoController {
 		return empleadoService.save(empleado);
 	}
 	
+	@GetMapping("/all")
+	public List<Empleado> getEmpleados() {
+		return empleadoService.getEmpleados();
+	}
+	
 	@GetMapping
 	public List<Empleado> getEmpleadoStartsWith(@RequestParam("nombre") String nombre) {
 		return empleadoService.getEmpleadoStartsWith(nombre);
@@ -35,6 +41,11 @@ public class EmpleadoController {
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable String id) {
 		empleadoService.delete(id);
+	}
+	
+	@PutMapping
+	public String update(@RequestBody Empleado empleado) {
+		return empleadoService.update(empleado);
 	}
 	
 }
